3968df93feb0c412ac3328f4239fc6768722488b
Chris Mc <prince.chrismc@gmail.com>
2018-09-25 19:20:48 -0400

    Passing var not ref

--- src/Interfaces.cpp
+++ src/Interfaces.cpp
@@ -43,7 +43,8 @@ Shader::IShader::IShader(const std::string& rel_path) : m_Status(false)
    else
    {
       std::cout << "Impossible to open " << rel_path.c_str() << ". Are you in the right directory ?" << std::endl;
-      std::getline(std::cin, std::string());
+      std::string input;
+      std::getline(std::cin, input);
    }
 }
 
